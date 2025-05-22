3e0ee4db8234cbb7facf2efb6b9e0a6b22187544
Felix08091380 <fhametgaleev@yandex.ru>
2025-05-22 20:42:04 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,24 @@
+const CACHE_NAME = 'notebook-cache-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Кэширование при установке
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('notebook-cache').then(cache =>
-      cache.addAll(['/', '/notebook.html', '/manifest.json', '/icon-192.png', '/icon-512.png'])
-    )
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
 });
 
+// Работа в оффлайне
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
